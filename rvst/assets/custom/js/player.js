c1ba4568e2a554e8a6af872f7488637bf59603b6
unknown <gabriel.costa@PMDPOA012.pmweb.local>
2015-08-04 11:34:28 -0300

    player funcionando

--- rvst/assets/custom/js/player.js
+++ rvst/assets/custom/js/player.js
@@ -1,9 +1,18 @@
-//Initialize the plugin
-$('.overlay').ttwMusicPlayer(playlist, {
+$(document).ready(function(){
+  var description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id tortor nisi. Aenean sodales diam ac lacus elementum scelerisque. Suspendisse a dui vitae lacus faucibus venenatis vel id nisl. Proin orci ante, ultricies nec interdum at, iaculis venenatis nulla. ';
+
+  //Initialize the plugin
+  $('.overlay').ttwMusicPlayer(playlist, {
+    description:description,
     currencySymbol:'$',
     buyText:'BUY',
     tracksToShow:5,
-    autoplay:false,
+    autoPlay:false,
+    jPlayer:{
+      swfPath:'assets/plugin/jquery-jplayer' //You need to override the default swf path any time the directory structure changes
+    },
     ratingCallback:function(index, playlistItem, rating){
             //some logic to process the rating, perhaps through an ajax call
-    } });
+    } 
+  });
+});
--- rvst/assets/custom/js/playlist.js
+++ rvst/assets/custom/js/playlist.js
@@ -1,12 +1,23 @@
 //Build your playlist. The playlist uses the same format s jPlayer (an array of objects. However, this plugin requires you to add some additional parameters. A sample playlist item:
-playlist = {
-    mp3:'mix/1.mp3',
-    oga:'mix/1.ogg',
-    rating:4.5,
-    title:'Some song title',
-    buy:'http://www.codebasehero.com',
-    price:'0.99',
-    duration:'4:28',
-    artist:'Artist Name',
-    cover:'images/cover.jpg'
-}
+var playlist = [{
+  mp3:'assets/custom/music/1.mp3',
+  oga:'assets/custom/music/1.ogg',
+  rating: 5,
+  title:'Failing HU3',
+  buy:'http://www.codebasehero.com',
+  price:'0.99',
+  duration:'4:28',
+  artist:'RVST',
+  cover:'assets/custom/img/rvst.jpg'
+},
+{
+  mp3:'assets/custom/music/1.mp3',
+  oga:'assets/custom/music/1.ogg',
+  rating: 5,
+  title:'Failing HU3',
+  buy:'http://www.codebasehero.com',
+  price:'0.99',
+  duration:'4:28',
+  artist:'RVST',
+  cover:'assets/custom/img/rvst.jpg'
+}];
