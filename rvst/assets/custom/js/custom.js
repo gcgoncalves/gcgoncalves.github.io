00099843d9685064fce77b5a20067da4c4c1a89a
unknown <gabriel.costa@PMDPOA012.pmweb.local>
2015-08-07 09:53:48 -0300

    seletor de bgfuncionando em mobile. :)

--- rvst/assets/custom/js/custom.js
+++ rvst/assets/custom/js/custom.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-	$(".bg-selector").on('change', function() {
+	$("#desktop-bg-selector").on('change', function() {
+  		//alert( this.value ); // or $(this).val()
+  		$('body').css('background', 'url(assets/custom/img/'+this.value+')');
+	});
+
+	$("#mobile-bg-selector").on('change', function() {
   		//alert( this.value ); // or $(this).val()
   		$('body').css('background', 'url(assets/custom/img/'+this.value+')');
 	});
