691d88021aa5a10e0730862daccd6a04000715cc
Gabriel Costa <gcgoncalves88@gmail.com>
2015-08-24 23:52:08 -0300

    analytics, mudança no lock do overlay, troca de fundo com f5

--- rvst/assets/custom/js/custom.js
+++ rvst/assets/custom/js/custom.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-	$(".bg-selector").on('change', function() {
-  		//alert( this.value ); // or $(this).val()
-  		$('body').css('background', 'url(assets/custom/img/'+this.value+')');
-	});
+	var backgrounds = ['bg_beige.jpg', 'bg_blue.jpg', 'bg_pink.jpg', 'bg_purple.jpg', 'bg_salmon.jpg']; //should add 'bg.jpg'?
+	var bg_index = Math.floor(Math.random() * backgrounds.length);
+	$('body').css('background', 'rgba(0, 0, 0, 0) url(assets/custom/img/'+backgrounds[bg_index]+') repeat fixed 50% center / cover');  		
 });
--- rvst/assets/custom/js/player.js
+++ rvst/assets/custom/js/player.js
@@ -13,10 +13,71 @@ $(document).ready(function(){
     }
   });
 
-  $('.jp-play').click(function() {
-    $('.overlay').toggleClass('lock');
-  });
-  $('.jp-pause').click(function() {
-    $('.overlay').toggleClass('lock');
-  });
+    $(".overlay").bind($.jPlayer.event.play, function(event) {
+        //playerTime grabs the current % location on the file being played.
+        //if they're at the beginning it's 0. If they're at the end it's 100. Etc.
+         var playerTime = Math.round(event.jPlayer.status.currentPercentAbsolute);
+        //grabs the media currently being played. Usefull for when multiple files are played in the player.
+        var mediaName = event.jPlayer.status.src;
+        
+        $('.overlay').toggleClass('lock');
+        
+        // Show lyrics
+        
+        //track it as an event with category:jPlayer, action:Play, label:Name of the file being played, value:location on file as %
+        _gaq.push(['_trackEvent', 'jPlayer', 'Play',mediaName,playerTime]);
+    });
+    //listener for a pause click
+    $(".overlay").bind($.jPlayer.event.pause, function(event) {
+        //as above, grabbing the % location and media being played
+        var playerTime = Math.round(event.jPlayer.status.currentPercentAbsolute);
+        var mediaName = event.jPlayer.status.src;
+              
+        $('.overlay').toggleClass('lock');
+        //We'll only track the "pause" if the percent value is less than 100. This is because at 100%
+        //when the player ends, it will send a pause event with the end event.
+        //we don't need that duplication in GA
+        if(playerTime<100){
+          //tracking the pause with similar setup to the play event
+          _gaq.push(['_trackEvent', 'jPlayer', 'Pause',mediaName,playerTime]);
+        }
+    });
+    //listening for the user dragging the seek bar
+    $(".overlay").bind($.jPlayer.event.seeking, function(event) {
+        //as above, grabbing the % location and media being played
+        var playerTime = Math.round(event.jPlayer.status.currentPercentAbsolute);
+        var mediaName = event.jPlayer.status.src;
+        //tracking the seeking action similar to above
+        _gaq.push(['_trackEvent', 'jPlayer', 'Seeking',mediaName,playerTime]);
+    });
+    //listening for when the user has stopped dragging the seek bar
+    $(".overlay").bind($.jPlayer.event.seeked, function(event) {
+        //as above, grabbing the % location and media being played
+        var playerTime = Math.round(event.jPlayer.status.currentPercentAbsolute);
+        var mediaName = event.jPlayer.status.src;
+        //There's some overlap between the seeked and stopped events. When a user clicks
+        // the stop button it actually sends a "seek" to the 0 location. So if the seeked location is 0
+        // then we track it as a stop, if it's greater than 0, it was an actual seek.
+        if(playerTime>0){
+            //track the seeked event as above
+            _gaq.push(['_trackEvent', 'jPlayer', 'Seeked',mediaName,playerTime]);
+        }else{
+            //track the stopped event as above
+            _gaq.push(['_trackEvent', 'jPlayer', 'Stopped',mediaName,playerTime]);
+        }
+    });
+    //listening for an end ie file completion
+    $(".overlay").bind($.jPlayer.event.ended, function(event) {
+        //as above, grabbing the % location and media being played
+        //except when it ends we force the value as 100%, otherwise it shoots back as 0
+        var playerTime = 100;
+        var mediaName = event.jPlayer.status.src;
+        //track the End event as above.
+        _gaq.push(['_trackEvent', 'jPlayer', 'Ended',mediaName,playerTime]);
+    });
 });
+
+
+
+// USAR PARA PEGAR AS LETRAS!!!
+// var mediaName = event.jPlayer.status.src;
