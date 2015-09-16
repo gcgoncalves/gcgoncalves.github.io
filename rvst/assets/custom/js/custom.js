924e756bec2510e8f26e61ea14a2573b85ce1132
Gabriel Costa <gcgoncalves88@gmail.com>
2015-09-16 00:43:40 -0300

    botões release/contato

--- rvst/assets/custom/js/custom.js
+++ rvst/assets/custom/js/custom.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 	var backgrounds = ['bg_beige.jpg', 'bg_blue.jpg', 'bg_pink.jpg', 'bg_purple.jpg', 'bg_salmon.jpg']; //should add 'bg.jpg'?
 	var bg_index = Math.floor(Math.random() * backgrounds.length);
-	$('body').css('background', 'rgba(0, 0, 0, 0) url(assets/custom/img/'+backgrounds[bg_index]+') repeat fixed 50% center / cover');  		
+	$('body').css('background', 'rgba(0, 0, 0, 0) url(assets/custom/img/'+backgrounds[bg_index]+') repeat fixed 50% center / cover');
+
+  $('#release').mouseenter(function(){
+    $('.release-label').hide();
+    $('.lang-selector').show();
+  }).mouseleave(function(){
+    $('.release-label').show();
+    $('.lang-selector').hide();
+  });
 });
