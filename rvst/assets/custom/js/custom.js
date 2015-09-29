b3e9151e15c99507f92a84e199350c1a67b02831
Gabriel Costa <gcgoncalves88@gmail.com>
2015-09-29 20:44:54 -0300

    fixing css and js

--- rvst/assets/custom/js/custom.js
+++ rvst/assets/custom/js/custom.js
@@ -7,25 +7,36 @@ $(document).ready(function(){
     $('.release-label').hide();
     $('.lang-selector').show();
   }).mouseleave(function(){
-    $('.release-label').show();
     $('.lang-selector').hide();
+    $('.release-label').show();
   });
   
   $('#release-pt').click(function(){
     $('.release_img.en').hide();
     $('.release_box').show();
     $('.release_img.pt').show();
-    $('.home_body').addClass('noscroll');
+    $('body').addClass('noscroll');
+    ga('send', 'event', 'link', 'click', 'release-pt');
   });
   $('#release-en').click(function(){
     $('.release_img.pt').hide();
     $('.release_box').show();
     $('.release_img.en').show();
-    $('.home_body').addClass('noscroll');
+    $('body').addClass('noscroll');
+    ga('send', 'event', 'link', 'click', 'release-en');
   });
   $('.release_box').click(function(){
     $('.release_box').hide();
     $('.release_img').hide();
-    $('.home_body').removeClass('noscroll');
+    $('body').removeClass('noscroll');
+    ga('send', 'event', 'link', 'click', 'release-close');
+  });
+  
+  $('.contact').mouseenter(function(){
+    $('.contact-text').hide();
+    $('.contact-email').show();
+  }).mouseleave(function(){
+    $('.contact-email').hide();
+    $('.contact-text').show();
   });
 });
