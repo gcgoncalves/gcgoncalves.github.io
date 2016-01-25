ead784746b810430ee07d43230429b1b57a157af
Gabriel Costa <gcgoncalves88@gmail.com>
2016-01-25 20:37:43 -0200

    fixing layout

--- app/portifolio.js
+++ app/portifolio.js
@@ -2,11 +2,18 @@ var portifolioApp = angular.module('portifolioApp', ['ngRoute']);
 
 portifolioApp.controller('portifolioController', function ($scope) {});
 
-portifolioApp.config(function ($routeProvider) {
+portifolioApp.config(function ($routeProvider, $locationProvider) {
   $routeProvider
   .when('/', {
     controller: 'portifolioController',
     templateUrl: 'app/views/home.html'
   })
+  .when('/portifolio', {
+    controller: 'portifolioController',
+    templateUrl: 'app/views/portifolio.html'
+  })
   .otherwise({redirectTo: '/'});
+
+  // use the HTML5 History API
+  $locationProvider.html5Mode(true);
 });
