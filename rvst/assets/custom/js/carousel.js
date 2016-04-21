e0b9f0143289815547c4f2f907a3716300c9a93f
Gabriel Costa <gcgoncalves88@gmail.com>
2016-04-21 11:18:04 -0300

    adding gallery

---
+++ rvst/assets/custom/js/carousel.js
@@ -0,0 +1,92 @@
+var imageList = [
+    {
+        "image":"assets/custom/img/carousel/RVST_Dogstock_Arte_by_Ruben_Braccini.jpg",
+        "caption":"Ruben Braccini",
+        "link":"assets/custom/img/carousel/RVST_Dogstock_Arte_by_Ruben_Braccini.jpg",
+        "title":"Ruben Braccini"
+    },
+    {
+        "image":"assets/custom/img/carousel/RVST_Dogstock_Arte_by_Michelle_Matiotti.jpg",
+        "caption":"Michelle Matiotti",
+        "link":"assets/custom/img/carousel/RVST_Dogstock_Arte_by_Michelle_Matiotti.jpg",
+        "title":"Michelle Matiotti"
+    },
+    {
+        "image":"assets/custom/img/carousel/RVST_PREMIERE_FlyerArte_by_Rubben_Braccini.png",
+        "caption":"Rubben Braccini.png",
+        "link":"assets/custom/img/carousel/RVST_PREMIERE_FlyerArte_by_Rubben_Braccini.png",
+        "title":"Rubben Braccini.png"
+    },
+    {
+        "image":"assets/custom/img/carousel/RVST_ClubSilencio_Foto_by_MicheleMatiotti.JPG",
+        "caption":"assets/custom/img/carousel/RVST_ClubSilencio_Foto_by_MicheleMatiotti.JPG",
+        "link":"assets/custom/img/carousel/RVST_ClubSilencio_Foto_by_MicheleMatiotti.JPG",
+        "title":"assets/custom/img/carousel/RVST_ClubSilencio_Foto_by_MicheleMatiotti.JPG"
+    },
+    {
+        "image":"assets/custom/img/carousel/RVST_ClubSilencio_Foto_by_MicheleMatiotti_2.jpg",
+        "caption":"assets/custom/img/carousel/RVST_ClubSilencio_Foto_by_MicheleMatiotti_2.jpg",
+        "link":"assets/custom/img/carousel/RVST_ClubSilencio_Foto_by_MicheleMatiotti_2.jpg",
+        "title":"assets/custom/img/carousel/RVST_ClubSilencio_Foto_by_MicheleMatiotti_2.jpg"
+    },
+    {
+        "image":"assets/custom/img/carousel/RVST_ClubSilencio_Foto_by_MicheleMatiotti_3.jpg",
+        "caption":"assets/custom/img/carousel/RVST_ClubSilencio_Foto_by_MicheleMatiotti_3.jpg",
+        "link":"assets/custom/img/carousel/RVST_ClubSilencio_Foto_by_MicheleMatiotti_3.jpg",
+        "title":"assets/custom/img/carousel/RVST_ClubSilencio_Foto_by_MicheleMatiotti_3.jpg"
+    },
+    {
+        "image":"assets/custom/img/carousel/RVST_ClubSilencio_Foto_by_MicheleMatiotti_4.jpg",
+        "caption":"assets/custom/img/carousel/RVST_ClubSilencio_Foto_by_MicheleMatiotti_4.jpg",
+        "link":"assets/custom/img/carousel/RVST_ClubSilencio_Foto_by_MicheleMatiotti_4.jpg",
+        "title":"assets/custom/img/carousel/RVST_ClubSilencio_Foto_by_MicheleMatiotti_4.jpg"
+    },
+    {
+        "image":"assets/custom/img/carousel/RVST_foto_by_JorgeSaBrito.jpg",
+        "caption":"assets/custom/img/carousel/RVST_foto_by_JorgeSaBrito.jpg",
+        "link":"assets/custom/img/carousel/RVST_foto_by_JorgeSaBrito.jpg",
+        "title":"assets/custom/img/carousel/RVST_foto_by_JorgeSaBrito.jpg"
+    },
+    {
+        "image":"assets/custom/img/carousel/RVST_Margot_Foto_by_Michelle_Matiotti.jpg",
+        "caption":"assets/custom/img/carousel/RVST_Margot_Foto_by_Michelle_Matiotti.jpg",
+        "link":"assets/custom/img/carousel/RVST_Margot_Foto_by_Michelle_Matiotti.jpg",
+        "title":"assets/custom/img/carousel/RVST_Margot_Foto_by_Michelle_Matiotti.jpg"
+    },
+    {
+        "image":"assets/custom/img/carousel/RVST_Margot_Foto_by_Michelle_Matiotti_2.jpg",
+        "caption":"assets/custom/img/carousel/RVST_Margot_Foto_by_Michelle_Matiotti_2.jpg",
+        "link":"assets/custom/img/carousel/RVST_Margot_Foto_by_Michelle_Matiotti_2.jpg",
+        "title":"assets/custom/img/carousel/RVST_Margot_Foto_by_Michelle_Matiotti_2.jpg"
+    },
+    {
+        "image":"assets/custom/img/carousel/RVST_Margot_Foto_by_Michelle_Matiotti_3.jpg",
+        "caption":"assets/custom/img/carousel/RVST_Margot_Foto_by_Michelle_Matiotti_3.jpg",
+        "link":"assets/custom/img/carousel/RVST_Margot_Foto_by_Michelle_Matiotti_3.jpg",
+        "title":"assets/custom/img/carousel/RVST_Margot_Foto_by_Michelle_Matiotti_3.jpg"
+    },
+    {
+        "image":"assets/custom/img/carousel/RVST_Margot_foto_by_tamires_rodriguez.jpg",
+        "caption":"assets/custom/img/carousel/RVST_Margot_foto_by_tamires_rodriguez.jpg",
+        "link":"assets/custom/img/carousel/RVST_Margot_foto_by_tamires_rodriguez.jpg",
+        "title":"assets/custom/img/carousel/RVST_Margot_foto_by_tamires_rodriguez.jpg"
+    },
+    {
+        "image":"assets/custom/img/carousel/RVST_RevistaNoize_Foto_by_Ariel_Fagundes.jpg",
+        "caption":"assets/custom/img/carousel/RVST_RevistaNoize_Foto_by_Ariel_Fagundes.jpg",
+        "link":"assets/custom/img/carousel/RVST_RevistaNoize_Foto_by_Ariel_Fagundes.jpg",
+        "title":"assets/custom/img/carousel/RVST_RevistaNoize_Foto_by_Ariel_Fagundes.jpg"
+    },
+    {
+        "image":"assets/custom/img/carousel/RVST_TheatroSaoPedro_foto_by_Carolina_Goes.jpg",
+        "caption":"assets/custom/img/carousel/RVST_TheatroSaoPedro_foto_by_Carolina_Goes.jpg",
+        "link":"assets/custom/img/carousel/RVST_TheatroSaoPedro_foto_by_Carolina_Goes.jpg",
+        "title":"assets/custom/img/carousel/RVST_TheatroSaoPedro_foto_by_Carolina_Goes.jpg"
+    },
+    {
+        "image":"assets/custom/img/carousel/RVST_TSP.jpg",
+        "caption":"assets/custom/img/carousel/RVST_TSP.jpg",
+        "link":"assets/custom/img/carousel/RVST_TSP.jpg",
+        "title":"assets/custom/img/carousel/RVST_TSP.jpg"
+    }
+];
