4b0a1a96d76a7387212b24a78f111a2f55fb6b8d
Gabriel Costa <gcgoncalves88@gmail.com>
2016-08-20 19:28:21 -0300

    Applying angular to add clippings and gallery

---
+++ rvst/app/rvst.js
@@ -0,0 +1,56 @@
+var rvstApp = angular.module('rvstApp', ['ngRoute']);
+
+rvstApp.config(function ($routeProvider, $locationProvider) {
+  $routeProvider
+  .when('/', {
+    controller: 'homeController',
+    templateUrl: 'app/views/home.html'
+  })
+  .when('/clippings', {
+    controller: 'clippingController',
+    templateUrl: 'app/views/clippings.html'
+  })
+  .when('/gallery', {
+    controller: 'galleryController',
+    templateUrl: 'app/views/gallery.html'
+  })
+  .otherwise({redirectTo: '/'});
+
+  // use the HTML5 History API
+  $locationProvider.html5Mode(true);
+});
+
+var randomizeBackground = function(){
+    var width = $(window).width();
+    var backgrounds = ['bg_beige_sm.jpg', 'bg_blue_sm.jpg', 'bg_pink_sm.jpg', 'bg_purple_sm.jpg', 'bg_salmon_sm.jpg'];
+    if (width > 1199) {
+        backgrounds = ['bg_beige.jpg', 'bg_blue.jpg', 'bg_pink.jpg', 'bg_purple.jpg', 'bg_salmon.jpg'];
+    }
+    var bg_index = Math.floor(Math.random() * backgrounds.length);
+    $('body').css('background', 'rgba(0, 0, 0, 0) url(assets/custom/img/'+backgrounds[bg_index]+') repeat fixed 50% center / cover');
+};
+
+rvstApp.controller('homeController', function ($scope) {
+    randomizeBackground();
+});
+
+rvstApp.controller('clippingController', function ($scope, $http) {
+  $('body').css('background', 'rgba(0, 0, 0, 0) url(assets/custom/img/clippings/bg.png) repeat fixed 50% center / cover');
+
+  $scope.clippings= [];
+  $http.get('app/clippings.json').success(function(data) { 
+      $scope.clippings = data;
+  });
+});
+
+rvstApp.controller('galleryController', function ($scope) {
+    randomizeBackground();
+});
+
+rvstApp.directive('clippingItem', function () {
+  return {
+    scope: '=clippingItem',
+    restrict: 'EA',
+    templateUrl: 'app/views/clipping-item.html'
+    };
+});
--- rvst/assets/custom/js/custom.js
+++ rvst/assets/custom/js/custom.js
@@ -1,42 +1,30 @@
 $(document).ready(function(){
-	var backgrounds = ['bg_beige.jpg', 'bg_blue.jpg', 'bg_pink.jpg', 'bg_purple.jpg', 'bg_salmon.jpg']; //should add 'bg.jpg'?
-	var bg_index = Math.floor(Math.random() * backgrounds.length);
-	$('body').css('background', 'rgba(0, 0, 0, 0) url(assets/custom/img/'+backgrounds[bg_index]+') repeat fixed 50% center / cover');
+    $('#release').mouseenter(function(){
+        $('.release-label').hide();
+        $('.lang-selector').show();
+    }).mouseleave(function(){
+        $('.lang-selector').hide();
+        $('.release-label').show();
+    });
 
-  $('#release').mouseenter(function(){
-    $('.release-label').hide();
-    $('.lang-selector').show();
-  }).mouseleave(function(){
-    $('.lang-selector').hide();
-    $('.release-label').show();
-  });
-  
-  $('#release-pt').click(function(){
-    $('.release_img.en').hide();
-    $('.release_box').show();
-    $('.release_img.pt').show();
-    $('body').addClass('noscroll');
-    ga('send', 'event', 'link', 'click', 'release-pt');
-  });
-  $('#release-en').click(function(){
-    $('.release_img.pt').hide();
-    $('.release_box').show();
-    $('.release_img.en').show();
-    $('body').addClass('noscroll');
-    ga('send', 'event', 'link', 'click', 'release-en');
-  });
-  $('.release_box').click(function(){
-    $('.release_box').hide();
-    $('.release_img').hide();
-    $('body').removeClass('noscroll');
-    ga('send', 'event', 'link', 'click', 'release-close');
-  });
-  
-  $('.contact').mouseenter(function(){
-    $('.contact-text').hide();
-    $('.contact-email').show();
-  }).mouseleave(function(){
-    $('.contact-email').hide();
-    $('.contact-text').show();
-  });
+    $('#release-pt').click(function(){
+        $('.release_img.en').hide();
+        $('.release_box').show();
+        $('.release_img.pt').show();
+        $('body').addClass('noscroll');
+        ga('send', 'event', 'link', 'click', 'release-pt');
+    });
+    $('#release-en').click(function(){
+        $('.release_img.pt').hide();
+        $('.release_box').show();
+        $('.release_img.en').show();
+        $('body').addClass('noscroll');
+        ga('send', 'event', 'link', 'click', 'release-en');
+    });
+    $('.release_box').click(function(){
+        $('.release_box').hide();
+        $('.release_img').hide();
+        $('body').removeClass('noscroll');
+        ga('send', 'event', 'link', 'click', 'release-close');
+    });
 });
